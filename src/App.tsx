831dfaaa3b619edcf647f283a55561fe10fbb597
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2022-07-26 19:07:10 -0300

    feat: create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './routes'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Ignite Timer</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
